895941a2e9f217200bb3c64a3fbff4f346ef362c
Friesi <dl11@gmx.net>
2013-10-05 20:23:05 +0200

    Paddle lässt sich mal verschieben :D

--- RePong/src/at/frikiteysch/repong/MainActivity.java
+++ RePong/src/at/frikiteysch/repong/MainActivity.java
@@ -2,7 +2,10 @@ package at.frikiteysch.repong;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -11,13 +14,18 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    public void btnGameStartOnClick(View v) {
+    	Intent myIntent = new Intent(this, MovePaddle.class);
+        //myIntent.putExtra("key", value); //Optional parameters
+        this.startActivity(myIntent);
     }
     
 }
---
+++ RePong/src/at/frikiteysch/repong/MovePaddle.java
@@ -0,0 +1,113 @@
+package at.frikiteysch.repong;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MovePaddle extends Activity implements OnTouchListener {
+	 
+	ImageView paddle;
+	float lastX = 0, lastY = 0;
+	int displayWidth, displayHeight, paddleHalfWidth;
+	
+	@SuppressLint("NewApi")
+	@SuppressWarnings("deprecation")
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.game_field);
+	    
+	    //Intent intent = getIntent();
+	    //String value = intent.getStringExtra("key"); //if it's a string you stored.
+	
+	    paddle = (ImageView) findViewById(R.id.paddle);
+	    
+	    FrameLayout layout = (FrameLayout) findViewById(R.id.gameField);
+        layout.setOnTouchListener(this);
+        
+        Display display = getWindowManager().getDefaultDisplay(); 
+        
+        if (android.os.Build.VERSION.SDK_INT >= 13) {
+        	Point size = new Point();
+        	display.getSize(size);
+        	displayWidth = size.x;
+        	displayHeight = size.y;
+        }
+        else {
+        	displayWidth = display.getWidth();  // deprecated
+        	displayHeight = display.getHeight();  // deprecated
+        }
+        
+	}
+	
+	@Override 
+	public void onWindowFocusChanged (boolean hasFocus) 
+	{
+		paddleHalfWidth = paddle.getWidth() / 2;
+	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) 
+	{
+		switch (event.getAction())
+		{
+			case MotionEvent.ACTION_DOWN:
+	        {       
+	              // Here u can write code which is executed after the user touch on the screen 
+	        	//Toast.makeText(this, "down", Toast.LENGTH_LONG).show();
+	        	break; 
+	        }
+	        case MotionEvent.ACTION_UP:
+	        {             
+	               // Here u can write code which is executed after the user release the touch on the screen    
+	        	//Toast.makeText(this, "up", Toast.LENGTH_LONG).show();
+	            break;
+	        }
+	        case MotionEvent.ACTION_MOVE:
+	        {  
+	           // Here u can write code which is executed when user move the finger on the screen 
+	        	
+	        	FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) paddle.getLayoutParams();
+	        	/*
+	        	float tmpY = event.getY();
+	        	float tmpX = event.getX();
+	        	
+	        	if (tmpX < lastX)
+	        		params.leftMargin -= 10;
+	        	else
+	        		params.leftMargin += 10;*/
+	        	
+	        	params.gravity = Gravity.LEFT | Gravity.BOTTOM;
+	        	
+	        	float tmpX = event.getX();
+	        	int tmpCalc = (int) (tmpX - paddleHalfWidth);
+	        	
+	        	if (tmpCalc < 0)
+	        		params.leftMargin = 0;
+	        	else if (tmpCalc + paddleHalfWidth * 2 > displayWidth)
+	        		params.leftMargin = displayWidth - paddleHalfWidth * 2;
+	        	else	        	
+	        		params.leftMargin = tmpCalc;
+	        	
+	            //params.bottomMargin = (int) event.getY();
+	            paddle.setLayoutParams(params);
+	            paddle.invalidate();
+	            break;
+	        }
+	    }
+		
+		lastX = event.getX();
+		
+	    return true;
+	}
+}
