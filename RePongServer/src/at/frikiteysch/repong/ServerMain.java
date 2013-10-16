d3d73274d7ef4dad24d3f595f8f1cbe8c10831ae
Friesi <dl11@gmx.net>
2013-10-16 16:47:19 +0200

    Server erstellt und HelloWorld eingefügt :D

---
+++ RePongServer/src/at/frikiteysch/repong/ServerMain.java
@@ -0,0 +1,14 @@
+package at.frikiteysch.repong;
+
+public class ServerMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+	}
+
+}
