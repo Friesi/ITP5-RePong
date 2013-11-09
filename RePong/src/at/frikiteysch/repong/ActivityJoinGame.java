26006787a2273cd1c3f7da067c33a33320148abe
Dominik Schildorfer <dominik.schildorfer@gmail.com>
2013-11-09 14:59:43 +0100

    CommunicationCenter added with static functions sendComObjectToServer and recieveComObjectFromServer to use it in all other classes. ActivityJoinGame prepaired for implementing GameList listing.

--- RePong/src/at/frikiteysch/repong/ActivityJoinGame.java
+++ RePong/src/at/frikiteysch/repong/ActivityJoinGame.java
@@ -1,18 +1,23 @@
 package at.frikiteysch.repong;
 
+import java.util.Map;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 public class ActivityJoinGame extends Activity {
-
+	private Map<Integer, GameListInfo> gameList;
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_game);
+        
+        gameList = sendGameListRequest();
     }
 	
+
 	public void btnRefreshOnClick(View v) {
     	// TODO: noch zu implementieren
     }
@@ -20,4 +25,9 @@ public class ActivityJoinGame extends Activity {
 	public void btnBackOnClick(View v) {
 		ActivityJoinGame.super.onBackPressed();
     }
+
+	private Map<Integer, GameListInfo> sendGameListRequest() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
---
+++ RePong/src/at/frikiteysch/repong/communication/CommunicationCenter.java
@@ -0,0 +1,54 @@
+package at.frikiteysch.repong.communication;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.StreamCorruptedException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+
+public class CommunicationCenter {
+	
+	static public void sendComObjectToServer(Socket s, Object comObjectToSend)
+	{
+		try {
+			s = new Socket("10.0.2.2", 3456);
+			//"ec2-54-200-186-85.us-west-2.compute.amazonaws.com", 3456);
+        ObjectOutputStream out = new ObjectOutputStream(s.getOutputStream());
+        out.writeObject(comObjectToSend);
+        out.flush();
+        
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+	}
+	
+	static public Object recieveComObjectFromServer(Socket s){
+		// Answer from server
+        ObjectInputStream inputObject;
+		try {
+			inputObject = new ObjectInputStream(s.getInputStream());
+		
+			return inputObject.readObject();
+		
+		} catch (StreamCorruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+    }
+	
+}
