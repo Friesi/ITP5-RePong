74edab3f751c027c7570ee40d7886ab848f80608
rafikime <rafael.teyral@gmail.com>
2013-12-28 17:50:49 +0100

    added field class

---
+++ RePongComLibrary/src/at/frikiteysch/repong/defines/Field.java
@@ -0,0 +1,25 @@
+package at.frikiteysch.repong.defines;
+
+import java.io.Serializable;
+
+public class Field implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6718874644704647179L;
+	private double height;
+	private double width;
+	
+	public double getHeight() {
+		return height;
+	}
+	public void setHeight(double height) {
+		this.height = height;
+	}
+	public double getWidth() {
+		return width;
+	}
+	public void setWidth(double width) {
+		this.width = width;
+	}
+}
